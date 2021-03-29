4ed03a638e595dae5452dd120568953e5efa2c10
schufeli <dextter767@gmail.com>
2021-03-29 19:22:57 +0200

    Added MockData for development only

---
+++ view/SwissTransportView/Mock/MockData.cs
@@ -0,0 +1,23 @@
+﻿using Newtonsoft.Json;
+using SwissTransport.Models;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SwissTransportView.Mock
+{
+    public class MockData
+    {
+        public static Stations GetStations()
+        {
+            using (StreamReader reader = new StreamReader("Mock/stations.json"))
+            {
+                string json = reader.ReadToEnd();
+                return JsonConvert.DeserializeObject<Stations>(json);
+            }
+        }
+    }
+}
