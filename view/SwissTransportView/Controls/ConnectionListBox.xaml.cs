7085c4124b84d269ba84bcfdfe2eb4b39bfb02ec
schufeli <dextter767@gmail.com>
2021-03-30 21:53:00 +0200

    Added ConnectionListBox

---
+++ view/SwissTransportView/Controls/ConnectionListBox.xaml.cs
@@ -0,0 +1,18 @@
+﻿using SwissTransportView.ViewModels;
+using System.Windows.Controls;
+
+namespace SwissTransportView.Controls
+{
+    /// <summary>
+    /// Interaction logic for ConnectionListBox.xaml
+    /// </summary>
+    public partial class ConnectionListBox : UserControl
+    {
+        ConnectionListBoxViewModel vm = new ConnectionListBoxViewModel();
+        public ConnectionListBox()
+        {
+            InitializeComponent();
+            this.DataContext = vm;
+        }
+    }
+}
---
+++ view/SwissTransportView/ViewModels/ConnectionListBoxViewModel.cs
@@ -0,0 +1,25 @@
+﻿using SwissTransport.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SwissTransportView.ViewModels
+{
+    public class ConnectionListBoxViewModel : ViewModelBase
+    {
+        private List<Connection> connections;
+        
+        public List<Connection> Connections
+        {
+            get { return connections; }
+            set { connections = value; OnPropertyChanged("Connections"); }
+        }
+
+        public void LoadConnections(List<Connection> connections)
+        {
+            Connections = connections;
+        }
+    }
+}
