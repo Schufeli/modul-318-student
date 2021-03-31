d522f893fa1b68f4c45879424db7901babaec1fe
schufeli <dextter767@gmail.com>
2021-03-31 20:21:47 +0200

    Added EmailShareWindow and EmailShareWindowViewModel

--- view/SwissTransportView/Views/EmailShareWindow.xaml.cs
+++ view/SwissTransportView/Views/EmailShareWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using SwissTransportView.ViewModels;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -19,9 +20,11 @@ namespace SwissTransportView.Views
     /// </summary>
     public partial class EmailShareWindow : Window
     {
+        EmailShareWindowViewModel vm = new EmailShareWindowViewModel();
         public EmailShareWindow()
         {
             InitializeComponent();
+            this.DataContext = vm;
         }
     }
 }
