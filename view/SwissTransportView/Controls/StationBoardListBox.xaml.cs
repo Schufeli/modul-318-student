bb998e74586abeb7f82ae3d296c05cb9b63e9bc4
schufeli <dextter767@gmail.com>
2021-03-31 08:42:17 +0200

    Added StationBoardListBox Files

---
+++ view/SwissTransportView/Controls/StationBoardListBox.xaml.cs
@@ -0,0 +1,18 @@
+﻿using SwissTransportView.ViewModels;
+using System.Windows.Controls;
+
+namespace SwissTransportView.Controls
+{
+    /// <summary>
+    /// Interaction logic for StationBoardListBox.xaml
+    /// </summary>
+    public partial class StationBoardListBox : UserControl
+    {
+        StationBoardListBoxViewModel vm = new StationBoardListBoxViewModel();
+        public StationBoardListBox()
+        {
+            InitializeComponent();
+            this.DataContext = vm;
+        }
+    }
+}
---
+++ view/SwissTransportView/ViewModels/StationBoardListBoxViewModel.cs
@@ -0,0 +1,19 @@
+﻿using SwissTransport.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SwissTransportView.ViewModels
+{
+    class StationBoardListBoxViewModel : ViewModelBase
+    {
+        private List<StationBoard> stationBoards;
+        public List<StationBoard> StationBoards
+        {
+            get { return stationBoards; }
+            set { stationBoards = value; OnPropertyChanged("StationBoards"); }
+        }
+    }
+}
